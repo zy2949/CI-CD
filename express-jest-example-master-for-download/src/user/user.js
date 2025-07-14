d4031b184c7e55f52ffec1999d72be8955c7e069
zy2949 <82760510+zy2949@users.noreply.github.com>
2025-07-14 15:36:00 +0200

    Delete express-jest-example-master-for-download directory

--- express-jest-example-master-for-download/src/user/user.js
+++
@@ -1,6 +0,0 @@
-module.exports = (app) => {
-  app.get('/user', (req, res) => res.send('GET method'));
-  app.post('/user', (req, res) => res.send('POST method'));
-  app.delete('/user', (req, res) => res.send('DELETE method'));
-  app.put('/user', (req, res) => res.send('PUT method'));
-};
